481ef2e2f097220c14fc3230ef952d839e03aaf4
santiago.rey2 <santiago.rey2@udc.es>
2025-02-22 00:23:33 +0100

    First Commits Proyect Creation

---
+++ RetoGradiant/src/main/java/pase/gradiant/com/RetoGradiant/RetoGradiantApplication.java
@@ -0,0 +1,13 @@
+package pase.gradiant.com.RetoGradiant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoGradiantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoGradiantApplication.class, args);
+	}
+
+}
---
+++ RetoGradiant/src/test/java/pase/gradiant/com/RetoGradiant/RetoGradiantApplicationTests.java
@@ -0,0 +1,13 @@
+package pase.gradiant.com.RetoGradiant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoGradiantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
